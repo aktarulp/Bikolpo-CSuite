ee676d1a8bcc44884ab364e25b234fb478f2a4c9
Aktarul Pramanik <aktar.gub@gmail.com>
2025-10-23 04:15:43 +0600

    Extra Image Remove

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,7 +60,12 @@
     "publisher": {
       "@type": "Organization",
       "name": "Bikolpo Live",
-      "logo": "https://bikolpolive.com/images/BikolpoLive.png"
+      "logo": {
+        "@type": "ImageObject",
+        "url": "https://bikolpolive.com/images/BikolpoLive.png",
+        "width": 512,
+        "height": 512
+      }
     },
     "potentialAction": {
       "@type": "SearchAction",
@@ -72,7 +77,6 @@
 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-
 
 
 <body class="bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 text-gray-800 dark:text-gray-100 font-bangla">
